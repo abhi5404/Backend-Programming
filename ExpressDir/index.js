433a0bbd99ffb7ee9d9123c1198a268ef4413404
Abhijit Mondal <abhijitamp378@gmail.com>
2025-07-30 21:20:39 +0530

    updated

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -7,30 +7,35 @@ app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
 
-// Middleware to log every request
-app.use((req, res, next) => {
-    console.log("Request received");
-    next(); // Important to proceed to next middleware or route handler
-});
+// // Middleware to log every request
+// app.use((req, res, next) => {
+//     console.log("Request received");
+//     next(); // Important to proceed to next middleware or route handler
+// });
 
-// Routes
-app.get("/", (req, res) => {
-    res.send("You contacted root path");
-});
+// // Routes
+// app.get("/", (req, res) => {
+//     res.send("You contacted root path");
+// });
 
-app.get("/search", (req, res) => {
-    res.send("You contacted search path");
-});
+// app.get("/search", (req, res) => {
+//     res.send("i am good boy");
+// });
 
-app.get("/help", (req, res) => {
-    res.send("You contacted help path");
-});
+// app.get("/help", (req, res) => {
+//     res.send("You contacted help path");
+// });
 
-app.post("/", (req, res) => {
-    res.send("You sent a POST request");
-});
+// app.post("/", (req, res) => {
+//     res.send("You sent a POST request");
+// });
 
 app.get("/:username/:id", (req, res) => {
     let { username, id } = req.params;
     res.send(`Welcome to the page of @${username} with ID ${id}`);
+});
+
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    res.send(<h1>`You searched for: ${q}`</h1>);
 });
