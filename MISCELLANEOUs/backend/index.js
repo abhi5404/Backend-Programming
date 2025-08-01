f2bb20193159e50683251eec7ecc7595ba91d5fa
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-01 11:01:31 +0530

    get&post

--- MISCELLANEOUs/backend/index.js
+++ MISCELLANEOUs/backend/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080; 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.get("/register", (req, res) => {
     let {user , password} = req.query;  
     res.send(`standard GET request. Welcome ${user} to the site. Your password is ${password}`);
 });
 app.post("/register", (req, res) => {
+    let {user, password} = req.body;
+    console.log(req.body);
     res.send("standard POST request");
 });
 app.listen(port, () => {
