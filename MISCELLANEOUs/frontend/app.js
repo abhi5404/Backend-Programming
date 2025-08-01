29fa8a2976895aaa7bb2099f8e5fab7c5f497e68
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-01 13:19:46 +0530

    prototyping  const express = require("express");  const app = express();

---
+++ MISCELLANEOUs/frontend/app.js
@@ -0,0 +1,13 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+    
+}
+
+Person.prototype.talk = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+}
+
+let p1=new person("adam",25);
+let p2=new Person("eve",30);
