0b02777d746d4b52ad3c5e0dfe9f2e2fae22da98
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-02 11:03:25 +0530

    rest api

--- REST-CLASS/index.js
+++ REST-CLASS/index.js
@@ -4,9 +4,25 @@ const port = 8080;
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [
+    {
+        username:"abhi",
+        content:"this is my first post"
+    },
+    {
+        username:"sachin",
+        content:"this is my second post"
+    },
+    {
+        username:"priya",
+        content:"this is my third post"
+    }
+];
 
 app.get('/', (req, res) => {
     res.send("serving working well");
