403295084d9264a5d52d1fe1a99796ebea3fc595
Abhijit Mondal <abhijitamp378@gmail.com>
2025-08-15 23:17:09 +0530

    Add function to create random user data using Faker

--- SQL_Class/index.js
+++ SQL_Class/index.js
@@ -1,2 +1,14 @@
 const {faker} = require ('@faker-js/faker');
+let createRandomUser = () => {
+return {
+    userId: faker.string.uuid(),
+    userName: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate({min: 18, max: 65, mode: 'age'}),
+    registeredAt: faker.date.past(),
+};
+};
+console.log(createRandomUser());
 
+ 
