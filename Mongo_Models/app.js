29c62e614e1de96f7b52f69d7aa5be72584e5977
Abhijit Amp <abhijitamp378@gmail.com>
2025-12-30 21:16:58 +0530

    MONGO CONNECTED

--- Mongo_Models/app.js
+++ Mongo_Models/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const userModel = require('./user');
+const User = require('./user');
 const dbConnection = require('./config/db');
 
 const app = express();
 dbConnection();
 
+const createUser = async () => {
+  await User.create({
+    name: 'Abhijit',
+    email: 'abhijit@example.com',
+    age: 22
+  });
+  console.log('User inserted');
+};
+
+createUser();
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
 
